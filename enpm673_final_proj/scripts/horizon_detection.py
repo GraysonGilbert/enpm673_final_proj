4bff3eb2076de26045ab6ae21aaea88992e483d8
Grayson Gilbert <graysongilbert@gmail.com>
2025-05-03 16:03:11 -0400

    Horizon detection working. Removed old node

--- enpm673_final_proj/scripts/horizon_detection.py
+++ enpm673_final_proj/scripts/horizon_detection.py
@@ -11,12 +11,25 @@ class HorizonLine(Node):
   def __init__(self):
     super().__init__('optical_flow')
     # self.declare_parameter('use_sim_time', True)
+
+    # Convert raw ROS image to OpenCV type
+    self._bridge = CvBridge()
+
     # Subscribe to camera topic
     self._img_sub = self.create_subscription(
         Image,
         '/camera/image_raw',
         self.horizon_callback,
         5)
+    
+
+    # # Subscribe to camera topic (Turtlebot4)
+    # self._img_sub = self.create_subscription(
+    #     Image,
+    #     '/camera/image_raw',
+    #     self.horizon_callback,
+    #     5)
+
 
     # Publisher for the processed image
     self.image_pub = self.create_publisher(
@@ -25,10 +38,7 @@ class HorizonLine(Node):
         10
     )
     
-    # Convert raw ROS image to OpenCV type
-    self._bridge = CvBridge()
-    
-    self.checkerboard_dims = (7,5)
+    self.checkerboard_dims = (7,5) # Checkerboard Dimensions
 
     self.image_ready = True  # Flag to control image grabbing
     self.timer = self.create_timer(5.0, self.enable_next_image)
@@ -37,20 +47,21 @@ class HorizonLine(Node):
     self.image_ready = True
     self.get_logger().info("Ready to grab next image...")
 
-  # Subscription callback for performing optical flow detection and tracking
+  # Subscription callback for horizon line detection
   def horizon_callback(self,msg):
 
     if not self.image_ready:
         return
 
     self.image_ready = False  # Block until the next 5 seconds
-    self.get_logger().info("Image received and processed.")
+    #self.get_logger().info("Image received and processed.")
 
-    cv_image = self._bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+    cv_image = self._bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8') # Converting ROS message to OpenCV image
     
-    gray = cv.cvtColor(cv_image, cv.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(cv_image, cv.COLOR_BGR2GRAY) # Greyscale image
 
-    ret, corners = cv.findChessboardCorners(gray, (7,5), None)
+    # Detect and log checkerboard corners found
+    ret, corners = cv.findChessboardCorners(gray, self.checkerboard_dims, None)
     self.get_logger().info(f"findChessboardCorners returned: ret={ret}, number_of_corners={len(corners) if corners is not None else 0}")
 
     if ret:
@@ -60,12 +71,9 @@ class HorizonLine(Node):
         criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
         corners = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
 
-        # Reshape for easier indexing: shape (rows, columns, 2)
-        corners = corners.reshape(self.checkerboard_dims[1], self.checkerboard_dims[0], 2)
+        corners = corners.reshape(self.checkerboard_dims[1], self.checkerboard_dims[0], 2) # Reshape for easier indexing: shape (rows, columns, 2)
 
-        # Make a copy for drawing
-        image_with_lines = cv_image.copy()
-
+        image_with_lines = cv_image.copy() # Make a copy for drawing
 
         # Pick first and last rows of checkerboard
         row1_pts = (corners[0, 0], corners[0, -1])        # First row: first and last corner
@@ -76,11 +84,10 @@ class HorizonLine(Node):
         line2 = self.compute_line_equation(*row2_pts)
 
         # Compute their intersection (homogeneous coordinates)
-        vp = np.cross(line1, line2)
-        vp = vp / vp[2]  # Normalize to (x, y, 1)
+        vp_horizontal = np.cross(line1, line2)
+        vp_horizontal = vp_horizontal / vp_horizontal[2]  # Normalize to (x, y, 1)
 
-        # vp is now the **vanishing point** for horizontal lines
-        print(f"Vanishing point (horizontal lines): {vp}")
+        print(f"Vanishing point (horizontal lines): {vp_horizontal}")
 
 
         # Pick first and last columns of the checkerboard
@@ -105,32 +112,33 @@ class HorizonLine(Node):
             self.draw_extended_line(image_with_lines, pt1, pt2, color=(0, 255, 0), thickness=5)  # green for rows
 
         # 2 vertical lines (columns)
-        cols_to_draw = [0, self.checkerboard_dims[0]-1]  # first and last column
+        cols_to_draw = [0, self.checkerboard_dims[0]-1] 
         for col in cols_to_draw:
             pt1 = corners[0, col]
             pt2 = corners[-1, col]
             self.draw_extended_line(image_with_lines, pt1, pt2, color=(255, 0, 0), thickness=5)  # blue for columns
 
+        # # Draw horizon line (horizontal vanishing point)
+        # self.draw_horizontal_through_point(image_with_lines, vp_horizontal)
 
-        # Draw horizon line
+        # Draw horizon line (vertical vanishing point)
         self.draw_horizontal_through_point(image_with_lines, vp_vertical)
         
-        # Resize the image
-        resized_image = cv.resize(image_with_lines, (1280, 720), interpolation=cv.INTER_AREA)
         
-        # Convert the processed OpenCV image back to ROS Image message
-        processed_msg = self._bridge.cv2_to_imgmsg(resized_image, encoding="rgb8")
+        resized_image = cv.resize(image_with_lines, (1280, 720), interpolation=cv.INTER_AREA) # Resize the image
         
-        # Publish the processed image on a new topic
-        self.image_pub.publish(processed_msg)
+        processed_msg = self._bridge.cv2_to_imgmsg(resized_image, encoding="rgb8") # Convert the processed OpenCV image back to ROS Image message
+        
+        self.image_pub.publish(processed_msg) # Publish the processed image on a new topic
 
-        # Optionally, display the processed image in a window
+        # Display the processed image in a window
         cv.namedWindow("Processed Camera Feed", cv.WINDOW_NORMAL)
         cv.imshow("Processed Camera Feed", image_with_lines)
         cv.imwrite("screenshots/horizon_line_output.jpg", resized_image)
         cv.waitKey(1)
         
 
+  # Extend the parallel lines between checkerboard corners
   def draw_extended_line(self, img, pt1, pt2, color=(0, 255, 0), thickness=2):
         h, w = img.shape[:2]
         a = pt2[1] - pt1[1]
@@ -156,16 +164,15 @@ class HorizonLine(Node):
         if len(points) >= 2:
             cv.line(img, points[0], points[1], color, thickness)
 
-
+  # Computes the line coefficients between two points
   def compute_line_equation(self, p1, p2):
-        """Given two points, return line coefficients a, b, c for ax + by + c = 0"""
         a = p1[1] - p2[1]
         b = p2[0] - p1[0]
         c = p1[0]*p2[1] - p2[0]*p1[1]
         return np.array([a, b, c])
 
+  # Draw a horizontal line through a calculated vanishing point
   def draw_horizontal_through_point(self, image, vp, color=(255, 0, 0), thickness=5):
-        """Draw a horizontal line passing through the given vanishing point."""
         _, w = image.shape[:2]
 
         # Get the y-coordinate from the vanishing point
@@ -177,7 +184,6 @@ class HorizonLine(Node):
 
         cv.line(image, start_point, end_point, color, thickness)
 
-    
 
 # Spin node
 def main(args=None):
